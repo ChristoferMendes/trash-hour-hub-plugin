4e37278998f096e03ae05c17b5ce6741bdbb58cd
ChristoferMendes <christoferluizdsm@gmail.com>
2023-10-09 21:32:05 -0300

    refactor: Remove unused code for custom-card component

--- src/custom-card.tsx
+++ src/custom-card.tsx
@@ -2,18 +2,12 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import tailwindcss from "./index.css?inline";
 import react_awesome_button_css from "react-awesome-button/dist/styles.css?inline";
-import { extractCss } from "goober";
 
 export class TrashHourElement extends HTMLElement {
   connectedCallback() {
     const shadowRoot = this.attachShadow({ mode: "open" });
 
     const reactRoot = document.createElement("div");
-    const styleGoober = document.createElement("style");
-    styleGoober.textContent = extractCss();
-    styleGoober.setAttribute("id", "_goober");
-
-    reactRoot.appendChild(styleGoober);
 
     shadowRoot.appendChild(reactRoot);
 
