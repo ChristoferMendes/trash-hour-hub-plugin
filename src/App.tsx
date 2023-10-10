e48b737492e78731096af5c2392ca97cc7c9d727
ChristoferMendes <christoferluizdsm@gmail.com>
2023-10-10 13:05:36 -0300

    fix(App): Update import for Toaster

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import { ThemeProvider } from "./components/theme-provider";
 import { Check, Trash2 } from "lucide-react";
 import { AwesomeButton } from "react-awesome-button";
 import "react-awesome-button/dist/styles.css";
-import { Toaster, toast } from "react-hot-toast";
 import { useRequesrNotificationPermission } from "./hooks/use-request-notification-permission";
+import { Toaster, toast } from "sonner";
 
 export function App() {
   useRequesrNotificationPermission();
   return (
     <ThemeProvider>
       <div className="bg-background w-full">
-        <Toaster />
+        <Toaster position="top-center" />
         <section className="flex flex-col items-center justify-center space-y-12 h-screen">
           <h3 className="font-mono font-bold text-3xl">
             It's your time to take out the trash today!
