6145de0e15b99c7ca07136624099e767353096f3
ChristoferMendes <christoferluizdsm@gmail.com>
2023-10-09 21:24:22 -0300

    feat: Add goober and react-awesome-button styles

--- src/custom-card.tsx
+++ src/custom-card.tsx
@@ -1,18 +1,26 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import tailwindcss from "./index.css?inline";
+import react_awesome_button_css from "react-awesome-button/dist/styles.css?inline";
+import { extractCss } from "goober";
 
 export class TrashHourElement extends HTMLElement {
   connectedCallback() {
     const shadowRoot = this.attachShadow({ mode: "open" });
 
     const reactRoot = document.createElement("div");
+    const styleGoober = document.createElement("style");
+    styleGoober.textContent = extractCss();
+    styleGoober.setAttribute("id", "_goober");
+
+    reactRoot.appendChild(styleGoober);
 
     shadowRoot.appendChild(reactRoot);
 
     const css = new CSSStyleSheet({});
 
     css.replaceSync(tailwindcss);
+    css.replaceSync(react_awesome_button_css);
 
     shadowRoot.adoptedStyleSheets.push(css);
 
