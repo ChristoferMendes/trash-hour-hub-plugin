122fb6cc6db59312f74daa168a66b096182b2ffe
ChristoferMendes <christoferluizdsm@gmail.com>
2023-10-10 13:10:39 -0300

    feat: Add sonner CSS stylesheet

--- src/custom-card.tsx
+++ src/custom-card.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import tailwindcss from "./index.css?inline";
 import react_awesome_button_css from "react-awesome-button/dist/styles.css?inline";
+import sonner_css from "sonner/dist/styles.css?inline";
 
 export class TrashHourElement extends HTMLElement {
   connectedCallback() {
@@ -11,19 +12,22 @@ export class TrashHourElement extends HTMLElement {
 
     shadowRoot.appendChild(reactRoot);
 
-    const tailwindStyleSheet = new CSSStyleSheet({});
-
-    tailwindStyleSheet.replaceSync(tailwindcss);
-
-    const reactAwesomeButtonStyleSheet = new CSSStyleSheet({});
-
-    reactAwesomeButtonStyleSheet.replaceSync(react_awesome_button_css);
-
-    shadowRoot.adoptedStyleSheets.push(reactAwesomeButtonStyleSheet);
-    shadowRoot.adoptedStyleSheets.push(tailwindStyleSheet);
+    shadowRoot.adoptedStyleSheets.push(
+      this._injectStyle(tailwindcss),
+      this._injectStyle(react_awesome_button_css),
+      this._injectStyle(sonner_css)
+    );
 
     const root = createRoot(reactRoot);
 
     root.render(<App />);
+  }
+
+  _injectStyle(css: string) {
+    const cssStyleSheet = new CSSStyleSheet({});
+
+    cssStyleSheet.replaceSync(css);
+
+    return cssStyleSheet;
   }
 }
